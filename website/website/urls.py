5e23eb64e8371a736de67fdd0bf2feb19b441f7a
AllSafeCybercurity <skull32273227@gmail.com>
2023-04-22 14:19:46 -0700

    `em`

--- website/website/urls.py
+++ website/website/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('events.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Configure Admin Titles
+admin.site.site_header = "MMS Administration Page"
+admin.site.site_title = "Browser Title"
+admin.site.index_title = "Welcome To The Addmin Area..."
